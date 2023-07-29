ae0ab961cdbbcb7e0991abc40ec6f62d246e632a
dazehinn <dazehinn@yahoo.fr>
2023-07-29 20:03:29 +0100

    Update schema by migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_151921) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_180857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,9 +48,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_151921) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_counter"
   end
 
   add_foreign_key "comments", "posts"
